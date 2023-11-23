af7d4d9ae395ad6d1754d84e374d8144e057df5d
kingvadee <ojeifod8@gmail.com>
2023-11-23 16:19:49 +0100

    Updated files

---
+++ compiler.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Bash script for building a project using a Makefile.
+
+make re -f Makefile
+make oclean -f Makefile
---
+++ direct-compiler.sh
@@ -0,0 +1,3 @@
+src/*.c -Wall -Werror -Wextra -pedantic $(sdl2-config --cflags --libs) \
+	-o 'maze-project'
+# -lSDL2main -lSDL2
