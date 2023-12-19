3566a082613706abf97d920ab22f338edd15e1a9
Kingvadee <ojeifod8@gmail.com>
2023-12-18 16:53:48 -0800

    Updated repo

--- compiler.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Bash script for building a project using a Makefile.
-
-make re -f Makefile
-make oclean -f Makefile
--- direct-compiler.sh
+++
@@ -1,3 +0,0 @@
-src/*.c -Wall -Werror -Wextra -pedantic $(sdl2-config --cflags --libs) \
-	-o 'maze-project'
-# -lSDL2main -lSDL2
